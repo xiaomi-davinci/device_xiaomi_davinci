4643749aa084b10617657432bf1878ab6fd73029
Bruno Martins <bgcngm@gmail.com>
2019-08-20 21:47:56 +0100

    davinci: rootdir: Create power init script
    Gather all performance related items in one place
    and drop all deprecated services.
    
    Change-Id: I6e72f0799f1774f06ded7d0130d8a12e5772e227

--- device.mk
+++ device.mk
@@ -228,13 +228,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fstab.qcom \
     init.qcom.rc \
+    init.qcom.power.rc \
     init.qcom.usb.rc \
     init.recovery.qcom.rc \
     init.target.rc \
     ueventd.qcom.rc
 
 PRODUCT_PACKAGES += \
-    init.qcom.post_boot.sh \
     init.qcom.sh
 
 # IRSC
