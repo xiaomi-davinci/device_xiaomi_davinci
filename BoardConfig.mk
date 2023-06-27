bab299e377c4985f8217f29a6ca3c9e4071fc928
SebaUbuntu <barezzisebastiano@gmail.com>
2023-06-27 23:25:34 +0300

    davinci: Checkout NFC to LOS sm8150-common tree

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -66,12 +66,12 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
 DEVICE_MANIFEST_FILE += hardware/qcom-caf/sm8150/media/conf_files/sm6150/c2_manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/compatibility_matrix.xml
 
-ODM_MANIFEST_SKUS += davinci
-ODM_MANIFEST_DAVINCI_FILES := $(DEVICE_PATH)/configs/hidl/manifest-nfc.xml
+ODM_MANIFEST_SKUS += nfc
+ODM_MANIFEST_NFC_FILES := $(DEVICE_PATH)/configs/hidl/manifest-nfc.xml
 
 # Init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_davinci
-TARGET_RECOVERY_DEVICE_MODULES := libinit_davinci
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_davinci
+TARGET_RECOVERY_DEVICE_MODULES ?= init_xiaomi_davinci
 
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
